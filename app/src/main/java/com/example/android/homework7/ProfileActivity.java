1db31e31fe8bc5a448f1c4b791e5ebe4ebba69ce
bersabee17 <mjpecora@ncsu.edu>
2016-11-17 20:11:46 -0500

    Second Comming, 11/17/16 (20:10)

--- app/src/main/java/com/example/android/homework7/ProfileActivity.java
+++ app/src/main/java/com/example/android/homework7/ProfileActivity.java
@@ -2,12 +2,39 @@ package com.example.android.homework7;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private FrameLayout fragSwap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        findViewById(R.id.profile_users_btn).setOnClickListener(this);
+        findViewById(R.id.profile_messages_btn).setOnClickListener(this);
+        fragSwap = (FrameLayout) findViewById(R.id.profile_frame_layout);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.profile_users_btn:
+                break;
+            case R.id.profile_messages_btn:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void usersFragmentLoad() {
+
+    }
+
+    private void messagesFragmentLoad() {
+
     }
 }
